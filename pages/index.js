7a45ba756458f289b27fe7128db31bc17b418a9d
Sourav Adhikari <ad.sourav21@live.com>
2023-02-06 13:42:15 +0530

    updated favicon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Resilience Lab</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/resilience_lab_logo.svg" />
       </Head>
       <Navbar />
       <main >
